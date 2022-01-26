c1bfeceb2e9adb965656e77f8352add5d775190b
jdxj <985759262@qq.com>
2022-01-26 09:32:56 +0800

    test: varints

---
+++ proto-encoding/doc.go
@@ -0,0 +1,4 @@
+// 编译命令
+// protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative proto-encoding/simple-message/simple-message.proto
+
+package proto_encoding
